214bf24e41a339137d53dd249693602d64bf4e5a
José Luis <joalfe04@alumnatflorida.es>
2025-05-27 10:32:05 +0200

    Parte Programación Finalizada
    Clases programadas, listas para su uso.

--- src/main/c#/ecoEventos/Categoria.cs
+++ src/main/c#/ecoEventos/Categoria.cs
@@ -8,5 +8,21 @@ namespace ecoEventos
 {
     internal class Categoria
     {
+        // Atributos
+        private static int contId = 1;
+        private int id;
+        private string nombre;
+
+        // Constructor
+        public Categoria(int id, string nombre)
+        {
+            this.id = contId++;
+            this.nombre = nombre;
+        }
+
+        // Propiedades
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
     }
 }
--- src/main/c#/ecoEventos/Ubicacion.cs
+++ src/main/c#/ecoEventos/Ubicacion.cs
@@ -8,5 +8,23 @@ namespace ecoEventos
 {
     internal class Ubicacion
     {
+        // Atributos
+        private static int contId = 1;
+        private int id;
+        private string tipo;
+        private string direccion;
+
+        // Constructor
+        public Ubicacion(int id, string tipo, string direccion)
+        {
+            this.id = contId++;
+            this.tipo = tipo;
+            this.direccion = direccion;
+        }
+
+        // Propiedades
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
     }
 }
